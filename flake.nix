aa03816606e4aa8607d40e6c83ccfb0dedfdb9f8
rlkandela <rlkandela@gmail.com>
2021-05-26 16:57:00 +0200

    Initial commit
    Repository initialized with very basic configuration
    managed by nix-flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "rlkandela's NixOS System Configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-20.09"; 					# Input from official channel of nixpkgs
+    home-manager.url = "github:nix-community/home-manager/release-20.09"; 	# Input from github of home-manager
+    # Set the nixpkgs channel to follow the previously mentioned nixpkgs
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: 
+  let
+
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+      };
+    };
+  
+  lib = nixpkgs.lib;
+
+  in {
+	
+    homeConfigurations = {
+      rlkandela = home-manager.lib.homeManagerConfiguration {
+        inherit system pkgs;
+	username = "rlkandela";
+	homeDirectory = "/home/rlkandela";
+	configuration = {
+	  imports = [
+	    ./home/home.nix
+	  ];
+	};
+      };
+    };
+
+    nixosConfigurations = {
+      raul-workstation = lib.nixosSystem {
+        inherit system;
+
+	modules = [
+	  ./system/configuration.nix
+	];
+      };
+    };
+  };
+
+}
---
+++ home/home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "rlkandela";
+  home.homeDirectory = "/home/rlkandela";
+
+  home.packages = with pkgs; [
+    git git-crypt gitAndTools.gitflow
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "rlkandela";
+    userEmail = "rlkandela@gmail.com";
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.03";
+}
